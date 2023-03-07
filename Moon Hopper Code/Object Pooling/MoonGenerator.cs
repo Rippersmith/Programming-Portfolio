c9011cc2d68db5cd690936070dbe92c8f5e4b190
Rippersmith <30738656+Rippersmith@users.noreply.github.com>
2023-03-07 12:21:53 -0500

    Add files via upload

---
+++ Moon Hopper Code/Object Pooling/MoonGenerator.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonGenerator : MonoBehaviour {
+    public GameObject lastMoon;
+    public Sprite[] moonOverlays;
+    public Transform moonGenerationPoint;
+    public GameObject speedUp, slowDown, detonator;
+    public PlanetPooling moonPooler;
+
+    // Update is called once per frame
+    void Update() {
+        if (lastMoon.transform.position.x < transform.position.x)
+            SpawnNewMoon();
+
+        RandomPlanetSpawn();
+    }
+
+    //TODO: need to add an OverlapCircle safeguard to prevent this from spawning over the RandomPlanetSpawn() planets
+    void SpawnNewMoon()
+    {
+        int moonNum = Random.Range(0, 3);
+        //spawn the moon
+        GameObject newMoon = moonPooler.SpawnFromPool(moonNum, moonGenerationPoint.position, Quaternion.Euler(new Vector3(0, 0, Random.Range(0f, 360f))));
+
+        //make sure that this moon is far enough from the last moon that was spawned/position it in the right spot
+        float distance = (((lastMoon.transform.localScale.x * 2) + (newMoon.transform.localScale.x * 2)) * .1f) + .5f;
+        newMoon.transform.position = lastMoon.transform.position + (Vector3)MoveMoonToNewPoint(lastMoon.transform.position, Random.Range(-80f + (distance / 2), 80f - (distance / 2)), distance);
+
+        SpawnRandomItem(newMoon);
+        lastMoon = newMoon;
+    }
+
+    //use this to get the angle the moon will be from the last moon, and the distance
+    //this will happen if the moon overlaps with another existing moon
+    Vector2 MoveMoonToNewPoint(Vector2 currPos, float degrees, float radius)
+    {
+        Vector2 randomCircle = new Vector2(Mathf.Cos(degrees * Mathf.Deg2Rad), Mathf.Sin(degrees * Mathf.Deg2Rad));
+        Vector2 newPos = randomCircle * radius;
+
+        if (currPos.y + newPos.y > 4.5f)
+            return MoveMoonToNewPoint(currPos, DecreaseAngle(degrees, radius), radius);
+        else if (currPos.y + newPos.y < -4.5f)
+            return MoveMoonToNewPoint(currPos, IncreaseAngle(degrees, radius), radius);
+
+        return newPos;
+    }
+
+    //use this to decrease the angle the planet is at
+    float DecreaseAngle(float degrees, float distance)
+    {
+        float newDegrees = degrees;
+        newDegrees -= Random.Range(5f, 75f - distance);
+        return newDegrees;
+    }
+
+    float IncreaseAngle(float degrees, float distance)
+    {
+        float newDegrees = degrees;
+        newDegrees += Random.Range(5f, 75f - distance);
+        return newDegrees;
+    }
+
+    void SpawnRandomItem(GameObject newMoon)
+    {
+        int randomItemChance = Random.Range(6, 21); //20% chance of an item
+        //odds: 6: Speed Up   7: Slow Down   8: Instant Death
+        if (randomItemChance >= 8)
+        {       
+            GameObject newItem = moonPooler.SpawnItem(randomItemChance, newMoon.transform.position, Quaternion.Euler(new Vector3(0, 0, Random.Range(0f, 360f))), newMoon);
+        }
+    }
+
+    void RandomPlanetSpawn()
+    {
+        Vector2 randomPos = new Vector2(moonGenerationPoint.position.x + 2f, Random.Range(-4.5f, 4.5f));
+        float randomScale = Random.Range(8.5f, 26f);
+
+        if (!Physics2D.OverlapCircle(randomPos, (randomScale * .2f)))
+        {
+            int moonNum = Random.Range(3, 6);
+
+            //spawn the moon
+            GameObject newRandomMoon = moonPooler.SpawnFromPool(moonNum, randomPos, Quaternion.Euler(new Vector3(0, 0, Random.Range(0f, 360f))));
+
+            if (!Physics2D.OverlapCircle(randomPos, (randomScale * .2f)))
+                newRandomMoon.SetActive(false);
+            else
+                SpawnRandomItem(newRandomMoon);
+        }
+    }
+}
---
+++ Moon Hopper Code/Object Pooling/PlanetPooling.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetPooling : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool {
+        public int tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<int, Queue<GameObject>> moonPool;
+    float randomScaleMin = 8.5f, randomScaleMax = 26f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moonPool = new Dictionary<int, Queue<GameObject>>(); 
+
+        foreach(Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+            moonPool.Add(pool.tag, objectPool);
+        }
+    }
+
+    public GameObject SpawnFromPool(int poolTag, Vector3 spawnPosition, Quaternion spawnRotation)
+    {
+        if (!moonPool.ContainsKey(poolTag))
+        {
+            return null;
+        }
+
+        GameObject objectToSpawn = moonPool[poolTag].Dequeue();
+        MoonGravity moonScript = objectToSpawn.GetComponent<MoonGravity>();
+
+        //"instantiate" the moon
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = spawnPosition;
+        objectToSpawn.transform.rotation = spawnRotation;
+
+        //randomize the size of the moon
+        float randomScale = Random.Range(randomScaleMin, randomScaleMax);
+        objectToSpawn.transform.localScale = Vector2.one * randomScale;
+
+        //randomly change the colors of the 2 layers of the moon spawned
+        moonScript.moonMain.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        moonScript.moonOverlay.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+
+        //re-queue the moon
+        moonPool[poolTag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+
+    public GameObject SpawnItem(int poolTag, Vector3 spawnPosition, Quaternion spawnRotation, GameObject parentMoon)
+    {
+        if (!moonPool.ContainsKey(poolTag))
+        {
+            return null;
+        }
+
+        //"instantiate" the item
+        GameObject itemToSpawn = moonPool[poolTag].Dequeue();
+        itemToSpawn.SetActive(true);
+
+        //set rotation of the item
+        itemToSpawn.transform.rotation = spawnRotation;
+
+        //set the position of the item at the moon's center, then move it to the edge
+        itemToSpawn.transform.position = spawnPosition + (itemToSpawn.transform.up * (parentMoon.gameObject.transform.localScale.x * .1f)) + (itemToSpawn.transform.up * (itemToSpawn.transform.localScale.y * .15f));
+
+        //make the item a parent of the moon
+        itemToSpawn.transform.parent = parentMoon.transform;
+
+        //re-queue the item
+        moonPool[poolTag].Enqueue(itemToSpawn);
+
+        return itemToSpawn;
+    }
+
+}
